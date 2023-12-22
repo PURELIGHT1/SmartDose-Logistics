17d1832b0a65d0a85a692e91130a058af20c8a0a
PURELIGHT1 <88739434+PURELIGHT1@users.noreply.github.com>
2023-12-23 01:54:02 +0700

    test(produk): actived push in master branch

--- src/pages/user/Produk/index.jsx
+++ src/pages/user/Produk/index.jsx
@@ -5,7 +5,7 @@ const Produk = () => {
             <div className="col-12">
                 <div className="card">
                     <h5>Empty Page</h5>
-                    <p>Use this page to start from scratch and place your custom content.</p>
+                    <p>Tes Branch</p>
                 </div>
             </div>
         </div>
